d95061a0b35fd907c5dc7a8fdb7fcaadabd54127
MaysWind <sunyunchen@gmail.com>
2014-07-23 16:58:33 +0800

    增加图书馆调用方法

---
+++ SDNUMobile.SDK/RestMethod/Library/GetIllegalList.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+using SDNUMobile.SDK.Entity.Library;
+
+namespace SDNUMobile.SDK.RestMethod.Library
+{
+    /// <summary>
+    /// 图书馆违章信息获取方法
+    /// </summary>
+    public class GetIllegalList : AbstractRestMethod
+    {
+        #region 属性
+        /// <summary>
+        /// 获取方法路径
+        /// </summary>
+        public override String MethodPath
+        {
+            get { return "library/getillegallist"; }
+        }
+
+        /// <summary>
+        /// 获取返回实体类型
+        /// </summary>
+        public override Type ResultEntityType
+        {
+            get { return typeof(LibraryIllegalInfo[]); }
+        }
+
+        /// <summary>
+        /// 获取或设置开始日期
+        /// </summary>
+        public DateTime? StartTime
+        {
+            get { return this.GetParameterDateTimeValue("start"); }
+            set { this.SetParameter("start", value.Value); }
+        }
+
+        /// <summary>
+        /// 获取或设置结束日期
+        /// </summary>
+        public DateTime? EndTime
+        {
+            get { return this.GetParameterDateTimeValue("end"); }
+            set { this.SetParameter("end", value.Value); }
+        }
+
+        /// <summary>
+        /// 获取或设置返回数量
+        /// </summary>
+        public Int32? Count
+        {
+            get { return this.GetParameterInt32Value("count"); }
+            set { this.SetParameter("count", value.Value); }
+        }
+
+        /// <summary>
+        /// 获取或设置返回页码
+        /// </summary>
+        public Int32? Index
+        {
+            get { return this.GetParameterInt32Value("index"); }
+            set { this.SetParameter("index", value.Value); }
+        }
+        #endregion
+    }
+}
