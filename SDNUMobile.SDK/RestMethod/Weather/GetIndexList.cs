c6a514a7c672df0b765cfd7d719a667d1f17232b
MaysWind <sunyunchen@gmail.com>
2014-07-24 01:00:22 +0800

    构造函数和验证增加区域ID

--- SDNUMobile.SDK/RestMethod/Weather/GetIndexList.cs
+++ SDNUMobile.SDK/RestMethod/Weather/GetIndexList.cs
@@ -35,5 +35,35 @@ namespace SDNUMobile.SDK.RestMethod.Weather
             set { this.SetParameter("areaid", value); }
         }
         #endregion
+
+        #region 构造方法
+        /// <summary>
+        /// 初始化新的天气指数信息获取方法
+        /// </summary>
+        /// <param name="areaID">区域ID</param>
+        public GetIndexList(String areaID)
+        {
+            this.AreaID = areaID;
+        }
+        #endregion
+
+        #region 方法
+        /// <summary>
+        /// 尝试验证方法内参数
+        /// </summary>
+        /// <param name="error">错误信息（如果存在）</param>
+        /// <returns>参数验证是否通过</returns>
+        public override Boolean TryValidateParameters(out String error)
+        {
+            if (String.IsNullOrEmpty(this.AreaID))
+            {
+                error = "区域ID不能为空！";
+                return false;
+            }
+
+            error = String.Empty;
+            return true;
+        }
+        #endregion
     }
 }
