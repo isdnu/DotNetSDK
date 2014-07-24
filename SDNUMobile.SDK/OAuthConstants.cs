7ce3e78a0203418330f87fa6dbfe7ba9650054d4
MaysWind <sunyunchen@gmail.com>
2014-07-24 15:36:14 +0800

    修改为内部类

--- SDNUMobile.SDK/OAuthConstants.cs
+++ SDNUMobile.SDK/OAuthConstants.cs
@@ -5,95 +5,95 @@ namespace SDNUMobile.SDK
     /// <summary>
     /// OAuth常量类
     /// </summary>
-    public static class OAuthConstants
+    internal static class OAuthConstants
     {
         /// <summary>
         /// 获取OAuth验证参数头
         /// </summary>
-        public const String AuthorizationOAuthHeader = "OAuth ";
+        internal const String AuthorizationOAuthHeader = "OAuth ";
 
         #region 参数名
         /// <summary>
         /// 获取应用Key参数名
         /// </summary>
-        public const String ConsumerKeyParameter = "oauth_consumer_key";
+        internal const String ConsumerKeyParameter = "oauth_consumer_key";
 
         /// <summary>
         /// 获取单次值参数名
         /// </summary>
-        public const String NonceParameter = "oauth_nonce";
+        internal const String NonceParameter = "oauth_nonce";
 
         /// <summary>
         /// 获取回调地址参数名
         /// </summary>
-        public const String CallbackParameter = "oauth_callback";
+        internal const String CallbackParameter = "oauth_callback";
 
         /// <summary>
         /// 获取验证码参数名
         /// </summary>
-        public const String VerifierParameter = "oauth_verifier";
+        internal const String VerifierParameter = "oauth_verifier";
 
         /// <summary>
         /// 获取签名方法参数名
         /// </summary>
-        public const String SignatureMethodParameter = "oauth_signature_method";
+        internal const String SignatureMethodParameter = "oauth_signature_method";
 
         /// <summary>
         /// 获取签名内容参数名
         /// </summary>
-        public const String SignatureParameter = "oauth_signature";
+        internal const String SignatureParameter = "oauth_signature";
 
         /// <summary>
         /// 获取时间戳参数名
         /// </summary>
-        public const String TimestampParameter = "oauth_timestamp";
+        internal const String TimestampParameter = "oauth_timestamp";
 
         /// <summary>
         /// 获取版本参数名
         /// </summary>
-        public const String VersionParameter = "oauth_version";
+        internal const String VersionParameter = "oauth_version";
 
         /// <summary>
         /// 获取令牌内容参数名
         /// </summary>
-        public const String TokenParameter = "oauth_token";
+        internal const String TokenParameter = "oauth_token";
 
         /// <summary>
         /// 获取令牌密钥参数名
         /// </summary>
-        public const String TokenSecretParameter = "oauth_token_secret";
+        internal const String TokenSecretParameter = "oauth_token_secret";
 
         /// <summary>
         /// 获取XAuth认证方式参数名
         /// </summary>
-        public const String AuthModeParameter = "x_auth_mode";
+        internal const String AuthModeParameter = "x_auth_mode";
 
         /// <summary>
         /// 获取XAuth认证用户密码参数名
         /// </summary>
-        public const String AuthPasswordParameter = "x_auth_password";
+        internal const String AuthPasswordParameter = "x_auth_password";
 
         /// <summary>
         /// 获取XAuth认证用户名参数名
         /// </summary>
-        public const String AuthUsernameParameter = "x_auth_username";
+        internal const String AuthUsernameParameter = "x_auth_username";
         #endregion
 
         #region 参数内容
         /// <summary>
         /// 获取支持的签名方法
         /// </summary>
-        public const String SupportSignatureMethod = "HMAC-SHA1";
+        internal const String SupportSignatureMethod = "HMAC-SHA1";
 
         /// <summary>
         /// 获取支持的XAuth认证方式
         /// </summary>
-        public const String SupportXAuthMode = "client_auth";
+        internal const String SupportXAuthMode = "client_auth";
 
         /// <summary>
         /// 获取当前使用的OAuth的版本
         /// </summary>
-        public const String CurrentVersion = "1.0";
+        internal const String CurrentVersion = "1.0";
         #endregion
     }
 }
